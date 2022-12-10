2d33e4314e014336c1ade22b0af32acbdaa4be2b
JenniferChae <chaec@sas.upenn.edu>
2022-12-10 14:47:57 -0500

    put back password

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 conn = pymysql.connect(host='localhost',
                        port = 3306,
                        user='root',
-                       password='rootroot',
+                       password='root',
                        db='FlaskDemo',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
