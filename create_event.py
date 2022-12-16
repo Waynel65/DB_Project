9aafc5c08b5f0eec3264efb03d2608dd4368a3ef
SumingZhang <38039801+SumingZhang@users.noreply.github.com>
2022-12-15 22:00:36 -0500

    create event py and html

---
+++ create_event.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, session, url_for, redirect, flash
+from app import app, conn
+@app.route('/create_event', methods=['GET','POST'])
+def create_event():
+    GroupName = request.args.get('gName')
+    return render_template("create_event.html")
+
+@app.route('/event_detail', methods=['GET','POST'])
+def event_detail():
+    user = session['username']
+    GroupName = request.form['GroupName']
+    eventName=request.form['eventName']
+    eventDesc=request.form['eventDescription']
+
+    cursor = conn.cursor();
+    date_time='SELECT now();'
+    cursor.execute(date_time)
+    data = cursor.fetchone()
+
+    query_1 = 'INSERT INTO FlaskDemo.Event (eName, eDesc,eDate,gName,gCreator) values (%s,%s, %s,%s, %s)'
+    cursor.execute(query_1, (eventName, eventDesc,data,GroupName,user))
+    conn.commit()
+    cursor.close()
+
+    return redirect('/dashboard')
