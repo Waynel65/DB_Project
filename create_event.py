dd06ec901cb6cbd32dad70094b22c35101ff3447
SumingZhang <38039801+SumingZhang@users.noreply.github.com>
2022-12-15 22:19:36 -0500

    minor changes

--- create_event.py
+++ create_event.py
@@ -9,16 +9,15 @@ def create_event():
 def event_detail():
     user = session['username']
     GroupName = request.form['gName']
-    eventName=request.form['eventName']
+    eventName=request.form['eventsName']
     eventDesc=request.form['eventDescription']
 
     cursor = conn.cursor();
     date_time='SELECT now();'
     cursor.execute(date_time)
     data = cursor.fetchone()
-
     query_1 = 'INSERT INTO FlaskDemo.Event (eName, eDesc,eDate,gName,gCreator) values (%s,%s, %s,%s, %s)'
-    cursor.execute(query_1, (eventName, eventDesc,data,GroupName,user))
+    cursor.execute(query_1, (eventName, eventDesc,data['now()'],GroupName,user))
     conn.commit()
     cursor.close()
 
